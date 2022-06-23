5c967c32d6720268e015f0c5abbd8833afc610f5
abner.wanyan <abner.wanyan@gmail.com>
2022-06-23 22:48:18 +0800

    调整base64

--- src/views/Base64/index.vue
+++ src/views/Base64/index.vue
@@ -1,51 +1,28 @@
 <template>
   <a-row :gutter="20">
-    <a-col :span="10">
-      <a-textarea
-        v-model:value="input"
-        :auto-size="{ minRows: 27, maxRows: 30 }"
-      />
+    <a-col :span="12">
+      <a-textarea v-model:value="decodeStr" :auto-size="{ minRows: 27, maxRows: 30 }" />
     </a-col>
-    <a-col :span="4" :style="{ marginTop: 'auto', marginBottom: 'auto' }">
-      <div>
-        <div>
-          <a-button @click="encodeStr" type="primary">编码</a-button>
-        </div>
-        <div :style="{ marginTop: '20px' }">
-          <a-button @click="decodeStr" type="primary">解码</a-button>
-        </div>
-      </div>
-    </a-col>
-    <a-col :span="10">
-      <a-textarea
-        v-model:value="output"
-        :auto-size="{ minRows: 27, maxRows: 30 }"
-      />
+    <a-col :span="12">
+      <a-textarea v-model:value="encodeStr" :auto-size="{ minRows: 27, maxRows: 30 }" />
     </a-col>
   </a-row>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, watch } from "vue";
 // import { invoke } from "@tauri-apps/api/tauri";
 import { Base64 } from "js-base64";
 
-const input = ref("");
-const output = ref("");
+const decodeStr = ref("");
+const encodeStr = ref("");
 
-const encodeStr = () => {
-  output.value = Base64.encode(input.value);
-  // invoke("base64_encode", { input: input.value }).then(
-  //   (res) => (output.value = res)
-  // );
-};
-
-function decodeStr() {
-  output.value = Base64.decode(input.value);
-  // invoke("base64_decode", { input: input.value }).then(
-  //   (res) => (output.value = res)
-  // );
-}
+watch(decodeStr, (val) => {
+  encodeStr.value = Base64.encode(val)
+})
+watch(encodeStr, (val) => {
+  decodeStr.value = Base64.decode(val)
+})
 </script>
 
 <style scoped>
